f583f59a88ff03658e34941f48693acd61efab18
Xiaoliang Yu <328553173@qq.com>
2018-10-09 20:59:00 +0800

    change props to load next page

--- src/components/WebPDF/index.tsx
+++ src/components/WebPDF/index.tsx
@@ -7,24 +7,10 @@ const pdfjsViewer = require('../../../node_modules/pdfjs-dist/web/pdf_viewer.js'
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.550/pdf.worker.js';
 // the default params
 const DEFAULT_DESIRE_WIDTH=980;
-//atob() is used to convert base64 encoded PDF to binary-like data.
-const pdfData = atob(
-  'JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnkgcG9pbnQKPDwKICAvVHlwZSAvQ2F0YWxvZwog' +
-  'IC9QYWdlcyAyIDAgUgo+PgplbmRvYmoKCjIgMCBvYmoKPDwKICAvVHlwZSAvUGFnZXMKICAv' +
-  'TWVkaWFCb3ggWyAwIDAgMjAwIDIwMCBdCiAgL0NvdW50IDEKICAvS2lkcyBbIDMgMCBSIF0K' +
-  'Pj4KZW5kb2JqCgozIDAgb2JqCjw8CiAgL1R5cGUgL1BhZ2UKICAvUGFyZW50IDIgMCBSCiAg' +
-  'L1Jlc291cmNlcyA8PAogICAgL0ZvbnQgPDwKICAgICAgL0YxIDQgMCBSIAogICAgPj4KICA+' +
-  'PgogIC9Db250ZW50cyA1IDAgUgo+PgplbmRvYmoKCjQgMCBvYmoKPDwKICAvVHlwZSAvRm9u' +
-  'dAogIC9TdWJ0eXBlIC9UeXBlMQogIC9CYXNlRm9udCAvVGltZXMtUm9tYW4KPj4KZW5kb2Jq' +
-  'Cgo1IDAgb2JqICAlIHBhZ2UgY29udGVudAo8PAogIC9MZW5ndGggNDQKPj4Kc3RyZWFtCkJU' +
-  'CjcwIDUwIFRECi9GMSAxMiBUZgooSGVsbG8sIHdvcmxkISkgVGoKRVQKZW5kc3RyZWFtCmVu' +
-  'ZG9iagoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNSBmIAowMDAwMDAwMDEwIDAwMDAwIG4g' +
-  'CjAwMDAwMDAwNzkgMDAwMDAgbiAKMDAwMDAwMDE3MyAwMDAwMCBuIAowMDAwMDAwMzAxIDAw' +
-  'MDAwIG4gCjAwMDAwMDAzODAgMDAwMDAgbiAKdHJhaWxlcgo8PAogIC9TaXplIDYKICAvUm9v' +
-  'dCAxIDAgUgo+PgpzdGFydHhyZWYKNDkyCiUlRU9G');
 
 interface IProps {
   url:string,
+  data:string,
   page:number
 }
 interface IStates {
@@ -43,27 +29,36 @@ export default class WebPDF extends React.Component<IProps, IStates> {
       this.canvas=React.createRef();
     }
     public componentDidMount () {
-      // show only one pdf page
-      pdfjsLib.getDocument({
-        url:this.props.url
-      }).then((pdf)=>{
-        this.setState({ pdf },function(){
-           //render the pdf
-           this.renderPage();
-        });
-      })
-      // loaded the base64
-      // const loadingTask = pdfjsLib.getDocument({data:pdfData});
-      // loadingTask.promise.then((pdf)=>{
-      //   this.setState({ pdf },()=>{
-      //     this.renderPage();
-      //   })
-      // })
+      const { url,data } = this.props;
+      if(url){
+        //fetch pdf and render
+        pdfjsLib.getDocument({ url }).then((pdf)=>{
+          this.setState({ pdf },()=>{
+            this.renderPage();
+          });
+        })
+      }else{
+        //loaded the base64
+        const loadingTask = pdfjsLib.getDocument({data});
+        loadingTask.promise.then((pdf)=>{
+          this.setState({ pdf },()=>{
+            this.renderPage();
+          })
+        })
+      }
     }
     static getDerivedStateFromProps(props, state){
       return { ...state ,page:props.page};
     }
-    private renderPage(){
+    //in the new lifestyle we can use this in shouldComponentUpdate
+    shouldComponentUpdate(nextProps, nextState){
+      const { pdf } = this.state;
+      if(nextProps.page!==this.state.page){
+         this.renderPage();
+      }
+      return true
+    }
+    private renderPage=()=>{
       const { pdf } = this.state;
       pdf.getPage(this.state.page).then((page) => {
         let desiredWidth = 375;
--- src/test.tsx
+++ src/test.tsx
@@ -1,6 +1,21 @@
 import * as React from "react";
 import {Component,Fragment} from 'react';
 import WebPDF from "./components/WebPDF/index";
+//atob() is used to convert base64 encoded PDF to binary-like data.
+const pdfData = atob(
+  'JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnkgcG9pbnQKPDwKICAvVHlwZSAvQ2F0YWxvZwog' +
+  'IC9QYWdlcyAyIDAgUgo+PgplbmRvYmoKCjIgMCBvYmoKPDwKICAvVHlwZSAvUGFnZXMKICAv' +
+  'TWVkaWFCb3ggWyAwIDAgMjAwIDIwMCBdCiAgL0NvdW50IDEKICAvS2lkcyBbIDMgMCBSIF0K' +
+  'Pj4KZW5kb2JqCgozIDAgb2JqCjw8CiAgL1R5cGUgL1BhZ2UKICAvUGFyZW50IDIgMCBSCiAg' +
+  'L1Jlc291cmNlcyA8PAogICAgL0ZvbnQgPDwKICAgICAgL0YxIDQgMCBSIAogICAgPj4KICA+' +
+  'PgogIC9Db250ZW50cyA1IDAgUgo+PgplbmRvYmoKCjQgMCBvYmoKPDwKICAvVHlwZSAvRm9u' +
+  'dAogIC9TdWJ0eXBlIC9UeXBlMQogIC9CYXNlRm9udCAvVGltZXMtUm9tYW4KPj4KZW5kb2Jq' +
+  'Cgo1IDAgb2JqICAlIHBhZ2UgY29udGVudAo8PAogIC9MZW5ndGggNDQKPj4Kc3RyZWFtCkJU' +
+  'CjcwIDUwIFRECi9GMSAxMiBUZgooSGVsbG8sIHdvcmxkISkgVGoKRVQKZW5kc3RyZWFtCmVu' +
+  'ZG9iagoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNSBmIAowMDAwMDAwMDEwIDAwMDAwIG4g' +
+  'CjAwMDAwMDAwNzkgMDAwMDAgbiAKMDAwMDAwMDE3MyAwMDAwMCBuIAowMDAwMDAwMzAxIDAw' +
+  'MDAwIG4gCjAwMDAwMDAzODAgMDAwMDAgbiAKdHJhaWxlcgo8PAogIC9TaXplIDYKICAvUm9v' +
+  'dCAxIDAgUgo+PgpzdGFydHhyZWYKNDkyCiUlRU9G');
 export default class PDFTest extends Component{
   state = {
     page:1
